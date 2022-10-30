4993ccab43f465a79cfb46f4cc95fa1536f89b14
qwh <15806293089@163.com>
2022-10-30 11:50:15 +0800

    watchEffect 和 oncleanup 消除异步影响，做了详细的记录

--- packages/runtime-core/src/apiWatch.ts
+++ packages/runtime-core/src/apiWatch.ts
@@ -2,7 +2,7 @@
  * @Author: qwh 15806293089@163.com
  * @Date: 2022-10-29 09:21:20
  * @LastEditors: qwh 15806293089@163.com
- * @LastEditTime: 2022-10-29 10:32:36
+ * @LastEditTime: 2022-10-30 11:48:56
  * @FilePath: /vue3-study/packages/runtime-core/src/apiWatch.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,8 +10,14 @@ import { isFunction, isObject } from "@vue/shared";
 import { isReactive } from '../../reactivity/src/reactive'
 import { ReactiveEffect } from "../../reactivity/src/effect";
 //第一个参数观察的源，第二个回调函数，三是配置的选项
-export function watch(source, cb, options) {
+export function watch(source, cb, options?) {
     return doWatch(source, cb, options)
+}
+
+//这的 effect 相当于 watch 里面的 source，是一个函数，用于重新计算，options是配置项
+export function watchEffect(effect, options?) {
+    //此方法没有回调函数
+    doWatch(effect, null, options);
 }
 
 function traverse(source, s = new Set()) {
@@ -32,22 +38,41 @@ function doWatch(source, cb, options) {
     let getter //稍微会传入 effect类 中作为第一个函数，
     if (isReactive(source)) { //是代理后的对象的情况下
         getter = () => traverse(source)
-    }else if(isFunction(source)){ //如果是函数，赋值到 getter
+    } else if (isFunction(source)) { //如果是函数，赋值到 getter
         getter = source
     }
 
     let oldValue;//用于存储老值
+    let cleanup;
+    //用户如果使用 onCleanup 会传入一个函数 ，用户在外部一旦调用这个函数那就好讲用户传入
+    //的函数赋值到内部的 cleanup 上，这上面就有值了
+    //onCleanup是源码中定义的一个函数，放到了 cb 的第三个参数上面
+    //一旦执行将用户传入的函数放到 cleanup变量上了
+    const onCleanup = (userCb) => {
+        cleanup = userCb;
+    }
     //定义调度函数
     const job = () => {
-        let newValue = effect.run() //return 执行的是 getter
-        cb(newValue,oldValue) //调用回调函数传入新值和老值
-        //然后将新值赋值到老值上
-        oldValue = newValue
+        if (cb) {
+            let newValue = effect.run() //return 执行的是 getter
+            //第一次执行的时候这个值 cleanup 是空的，因为还没执行 cb，cb 里面才会赋值
+            //如果 watch 观测的变量连续改，那么第二次执行的 cb的某段依赖传入变量的程序 会被屏蔽掉，因为这个时候 cleanup 已经有值了
+            //因为利用了闭包，所以第一次执行 cb 保存了第一次的 flag，第二次执行回调的时候有值（这个值是第一次的 包含flag的函数）
+            //将这个 flag 改 false，等到第一次的异步函数执行完，再执行 flag 标识下面程序的时候已经改完 flag，已经屏蔽掉了
+            if (cleanup) cleanup();
+            //用户传入的回调函数如果传入了三个参数，说明第三个对应 oncleanup
+            cb(newValue, oldValue, onCleanup) //调用回调函数传入新值和老值
+            //然后将新值赋值到老值上
+            oldValue = newValue
+        } else {
+            //没有回调的情况下，直接执行 getter
+            effect.run()
+        }
     }
     //getter是 fn，job 是 schedluer 回调函数
-    const effect = new ReactiveEffect(getter,job)
+    const effect = new ReactiveEffect(getter, job)
     //如果配置了 immediate 那就立即执行
-    if(options.immediate){
+    if (options?.immediate) {
         return job()
     }
 
--- packages/runtime-core/src/tests/apiWatch.spec.ts
+++ packages/runtime-core/src/tests/apiWatch.spec.ts
@@ -2,11 +2,11 @@
  * @Author: qwh 15806293089@163.com
  * @Date: 2022-10-29 10:38:02
  * @LastEditors: qwh 15806293089@163.com
- * @LastEditTime: 2022-10-29 16:57:52
+ * @LastEditTime: 2022-10-30 10:58:03
  * @FilePath: /vue3-study/packages/runtime-core/src/tests/apiWatch.spec.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { watch } from '../apiWatch'
+import { watch, watchEffect } from '../apiWatch'
 import { reactive } from '../../../reactivity/src/reactive'
 describe('watch', () => {
     it("watch", () => {
@@ -19,5 +19,51 @@ describe('watch', () => {
         }, { immediate: true });
         state.name = 'zf';
         expect(a).toBe(3);
+    })
+
+    it("watchEffect", () => {
+        const state = reactive({ flag: false, name: 'jw', age: 30 });
+        // watch 就是effect， 状态会收集watch effect， 属性变化后 会触发scheduler
+        let a = 1
+        watchEffect(() => {
+            a++
+            state.name
+        });
+        state.name = 'zf';
+        expect(a).toBe(3);
+    })
+
+    it("cleanup", () => {
+        const state = reactive({ flag: false, name: 'jw', age: 30 });
+        // 第一次接口速度比较慢  第二次比较快，但是用第一次的结果覆盖掉了第二次
+        let timer = 3000;
+        function getData(data) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(data)
+                }, timer -= 1000)
+            })
+        }
+
+        // 用户输入1 返回1  在输入2 返回2
+        let arr = []
+        let a:any = ''
+        watch(() => state.name, async function (newValue, oldValue, onCleanup) {
+            let flag = true;
+            onCleanup(() => { // 这个函数是一个闭包
+                flag = false;
+                // 清除token
+            })
+            let r = await getData(newValue);
+            if (flag) {
+                console.log(r)
+                a = r
+                expect(a).toBe(2);
+            }
+        });
+        // 什么叫闭包
+        state.name = 1;
+        state.name = 2;
+
     })
 })
